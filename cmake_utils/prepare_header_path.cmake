9b4187062c6333a8ba27726a440c15487241102f
life4gal <life4gal@gmail.com>
2023-02-07 10:49:40 +0800

    Replacing the path of the included header file now caches the mark variables so that they do not need to be processed each time the project is loaded multiple times.
    TODO: What do we do if I need to process a file again?

--- cmake_utils/prepare_header_path.cmake
+++ cmake_utils/prepare_header_path.cmake
@@ -1,6 +1,40 @@
 function(prepare_header_path)
-	function(do_replace directory_abs_path this_file_abs_path)
-		file(RELATIVE_PATH folder_name ${JANUS_SOURCE_FILES_PATH} ${directory_abs_path})
+	function(do_replace this_file_abs_path)
+		# .c or .h ?
+		string(REGEX MATCH "^.*[.]h$" this_file_is_header_file "${this_file_abs_path}")
+		if (NOT this_file_is_header_file)
+			string(REGEX MATCH "^.*[.]c$" this_file_is_source_file "${this_file_abs_path}")
+
+			if (NOT this_file_is_source_file)
+				message(FATAL_ERROR "Unsupported file [${this_file_abs_path}].")
+			endif (NOT this_file_is_source_file)
+		endif (NOT this_file_is_header_file)
+
+		message("Replacing the path of the included header file in the file [${this_file_abs_path}], this may take a while...")
+
+		# 3.20^
+		# cmake_path(GET ${this_file_abs_path} PARENT_PATH folder_name)
+		# 3.20v
+		get_filename_component(parent_path ${this_file_abs_path} DIRECTORY)
+
+		if (this_file_is_header_file)
+			file(RELATIVE_PATH folder_name ${JANUS_HEADER_FILES_PATH} ${parent_path})
+		else ()
+			file(RELATIVE_PATH folder_name ${JANUS_SOURCE_FILES_PATH} ${parent_path})
+		endif (this_file_is_header_file)
+		file(RELATIVE_PATH file_name ${parent_path} ${this_file_abs_path})
+
+		set(this_file_cache_entry_name JANUS_HEADER_PATH_REPLACED_${folder_name}_${file_name})
+
+		# already cached?
+		if ($CACHE{${this_file_cache_entry_name}})
+			return()
+		endif ($CACHE{${this_file_cache_entry_name}})
+
+		string(LENGTH ${folder_name} folder_name_length)
+		if (${folder_name_length} EQUAL 0)
+			message(FATAL_ERROR "The `replace included header path` function only supports files in nested folders. (Files in top-level directories do not need to be processed)")
+		endif (${folder_name_length} EQUAL 0)
 
 		# generate temp file for write
 		set(temp_file_path "${this_file_abs_path}.generated")
@@ -49,7 +83,10 @@ function(prepare_header_path)
 
 		# replace file
 		file(RENAME ${temp_file_path} ${this_file_abs_path})
-	endfunction(do_replace directory_abs_path this_file_abs_path)
+
+		# !!! CACHE THIS FILE !!!
+		set(${this_file_cache_entry_name} ON CACHE INTERNAL "Mark file ${this_file_abs_path} has been processed." FORCE)
+	endfunction(do_replace this_file_abs_path)
 
 	# =============================
 	# Necessary source files
@@ -57,11 +94,18 @@ function(prepare_header_path)
 
 	# plugins
 	# plugin.c
-	do_replace(${JANUS_SOURCE_FILES_PATH}/plugins ${JANUS_SOURCE_FILES_PATH}/plugins/plugin.c)
+	do_replace(${JANUS_SOURCE_FILES_PATH}/plugins/plugin.c)
 
 	# transport
 	# transport.c
-	do_replace(${JANUS_SOURCE_FILES_PATH}/transports ${JANUS_SOURCE_FILES_PATH}/transports/transport.c)
+	do_replace(${JANUS_SOURCE_FILES_PATH}/transports/transport.c)
+
+	# =============================
+	# Extra source files
+	# =============================
+	foreach (file IN LISTS JANUS_EXTRA_SOURCE_FILES)
+		do_replace(${file})
+	endforeach (file IN LISTS JANUS_EXTRA_SOURCE_FILES)
 endfunction(prepare_header_path)
 
 prepare_header_path()
