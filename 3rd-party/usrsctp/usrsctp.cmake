17ebab12ff5529110f7ff601f22e02b3bbca0256
life4gal <life4gal@gmail.com>
2023-02-21 11:04:11 +0800

    Now if there is no usrsctp.pc file it will still try to find usrsctp(.so).

--- 3rd-party/usrsctp/usrsctp.cmake
+++ 3rd-party/usrsctp/usrsctp.cmake
@@ -1,26 +1,38 @@
-pkg_check_modules(LIB_USRSCTP usrsctp)
+function(try_use_usrsctp)
+	pkg_check_modules(LIB_USRSCTP usrsctp)
 
-if (NOT LIB_USRSCTP_FOUND)
-	message(FATAL_ERROR "Cannot find usrsctp on your platform, install it first...")
-endif (NOT LIB_USRSCTP_FOUND)
+	if (NOT LIB_USRSCTP_FOUND)
+		find_system_library(
+				usrsctp
+				POST
+				REQUIRED
+		)
 
-janus_append_link_libraries(${LIB_USRSCTP_LIBRARIES})
-janus_append_link_directories(${LIB_USRSCTP_LIBRARY_DIRS})
-janus_append_link_libraries_name(${LIB_USRSCTP_LIBRARIES}-${LIB_USRSCTP_VERSION})
-janus_append_include_directories(${LIB_USRSCTP_INCLUDE_DIRS})
-janus_append_compile_flags(${LIB_USRSCTP_CFLAGS})
-janus_append_ld_flags(${LIB_USRSCTP_LDFLAGS})
+		if (NOT ${LIB_USRSCTP_FOUND})
+			message(FATAL_ERROR "Cannot find usrsctp on your platform, install it first...")
+		endif (NOT ${LIB_USRSCTP_FOUND})
+	endif (NOT LIB_USRSCTP_FOUND)
 
-include(CheckLibraryExists)
+	janus_append_link_libraries(${LIB_USRSCTP_LIBRARIES})
+	janus_append_link_directories(${LIB_USRSCTP_LIBRARY_DIRS})
+	janus_append_link_libraries_name(${LIB_USRSCTP_LIBRARIES}-${LIB_USRSCTP_VERSION})
+	janus_append_include_directories(${LIB_USRSCTP_INCLUDE_DIRS})
+	janus_append_compile_flags(${LIB_USRSCTP_CFLAGS})
+	janus_append_ld_flags(${LIB_USRSCTP_LDFLAGS})
 
-CHECK_LIBRARY_EXISTS(
-		usrsctp
-		usrsctp_finish
-		"${LIB_USRSCTP_LIBRARY_DIRS}"
-		LIB_USRSCTP_HAS_usrsctp_finish
-)
-if (LIB_USRSCTP_HAS_usrsctp_finish)
-	janus_append_compile_definitions(HAVE_SCTP)
-else ()
-	message(WARNING "usrsctp version does not have usrsctp_finish")
-endif (LIB_USRSCTP_HAS_usrsctp_finish)
+	include(CheckLibraryExists)
+
+	CHECK_LIBRARY_EXISTS(
+			usrsctp
+			usrsctp_finish
+			"${LIB_USRSCTP_LIBRARY_DIRS}"
+			LIB_USRSCTP_HAS_usrsctp_finish
+	)
+	if (LIB_USRSCTP_HAS_usrsctp_finish)
+		janus_append_compile_definitions(HAVE_SCTP)
+	else ()
+		message(WARNING "usrsctp version does not have usrsctp_finish")
+	endif (LIB_USRSCTP_HAS_usrsctp_finish)
+endfunction(try_use_usrsctp)
+
+try_use_usrsctp()
